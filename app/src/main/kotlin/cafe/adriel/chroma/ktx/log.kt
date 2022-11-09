7b5c8970aaacf5ed7b4c6cb04f4c4e896225a27f
Máté Martyin <mate.martyin@supercharge.io>
2022-11-09 22:33:22 +0100

    Disable firebase and google analytics

--- app/src/main/kotlin/cafe/adriel/chroma/ktx/log.kt
+++ app/src/main/kotlin/cafe/adriel/chroma/ktx/log.kt
@@ -1,6 +1,5 @@
 package cafe.adriel.chroma.ktx
 
-import com.google.firebase.crashlytics.FirebaseCrashlytics
+fun logError(error: Throwable) {
 
-fun logError(error: Throwable) =
-    FirebaseCrashlytics.getInstance().recordException(error)
+}
